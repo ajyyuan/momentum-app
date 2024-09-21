6e5d16f0b1ca9fba391fe23d6be02acd4819afcd
Andrew Yuan <andrewjyuan@gmail.com>
2024-09-21 16:19:19 -0400

    Added in logo

---
+++ frontend/src/components/MomentumLogo.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const MomentumLogo = () => {
+    return (
+        <Link to='/'>
+            <img src="img/Momentum_v4-bg.png" alt="Momentum logo" className="logo" />
+        </Link>
+    )
+}
+
+export default MomentumLogo
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
 
+import MomentumLogo from './MomentumLogo'
+
 const NavBar = () => {
     const { logout } = useLogout()
     const { user } = useAuthContext()
@@ -13,9 +15,9 @@ const NavBar = () => {
     return (
         <header>
             <div className="container">
-                <Link to="/">
-                    <h1>Momentum</h1>
-                </Link>
+                {/* TODO: allow guest session (if you refresh the page, your saved workouts will be gone! Click here to make an account)
+                */}
+                <MomentumLogo />
                 <nav>
                     {user && (
                         <div>
